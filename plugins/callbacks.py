df15e367021f554450b4587932f24084109e3fa2
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2021-11-11 18:46:46 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -4,6 +4,82 @@ import ast
 from pyrogram import Client as kinu
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+from translation import translation
+
+@kinu.on_callback_query()
+async def cd_handler(client, query):
+    
+    if query.data == "start_data":
+        await query.answer()
+        keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Command Help", callback_data="help_data")
+                ]
+            ]
+        )
+
+        await query.message.edit_text(
+            Script.START_MSG.format(query.from_user.mention),
+            reply_markup=keyboard,
+            disable_web_page_preview=True
+        )
+        return
+    
+    elif query.data ==  "help_user":
+        await query.answer()
+        keyboard = InlineKeyboardMarkup(
+            [
+                InlineKeyboardButton(
+                        "🤖 Updates", url="https://t.me/TMWAD"),
+                InlineKeyboardButton(
+                        "😊 About Me", callback_data="about_data")
+            ]
+        )
+        
+        await query.message.edit_text(
+            translation.help_user,
+            reply_markup=keyboard,
+            disable_web_page_preview=True
+        )
+        return
+    
+    elif query.data == "about_data":
+        await query.answer()
+        keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("🤖 Help", url="https://t.me/TMWAD"),
+                    InlineKeyboardButton(
+                        "🦸 Deverlpoer", url="https://github.com/kalanakt")
+                ],
+                [
+                    InlineKeyboardButton("BACK", callback_data="help_data"),
+                    InlineKeyboardButton("CLOSE", callback_data="close_data"),
+                ]
+            ]
+        )
+        
+        await query.message.edit_text(
+            translation.about_msg,
+            reply_markup=keyboard,
+            disable_web_page_preview=True
+        )
+        return
+    
+    elif query.data == "close_data":
+        await query.message.delete()
+       
+    
+    
+    
+            
+    
+    
+    
+            
+                
 
     
     
