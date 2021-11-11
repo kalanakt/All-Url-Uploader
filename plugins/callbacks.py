35e40a3a7ad2d07e2dd382202a7b156c11849225
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2021-11-11 23:33:54 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -12,7 +12,7 @@ async def cb_handler(client: kinu, query: CallbackQuery):
     if query.data == "help_user":
         buttons = [[
             InlineKeyboardButton('Updates', url="https://t.me/TMWAD"),
-            InlineKeyboardButton('Comments', url="https://t.me/TMWAD/17")
+            InlineKeyboardButton('About', callback_data='about')
             ],[
             InlineKeyboardButton('🏠 Home', callback_data='start'),
             InlineKeyboardButton('🔐 Close', callback_data='close_data')
@@ -26,6 +26,34 @@ async def cb_handler(client: kinu, query: CallbackQuery):
     
     elif query.data == "close_data":
         await query.message.delete()
+        
+       
+    elif query.data == "start":
+        buttons = [[
+            InlineKeyboardButton('Help', callback_data='help_user'),
+            InlineKeyboardButton('🤖 Updates', url="https://t.me/TMWAD")
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text=Translation.START_TEXT.format(query.from_user.mention),
+            reply_markup=reply_markup,
+            parse_mode='html'
+        )
+        
+       
+    if query.data == "about":
+        buttons = [[
+            InlineKeyboardButton('🦸 Deverloper', url='https://github.com/kalanakt')
+            ],[
+            InlineKeyboardButton('🏠 Home', callback_data='start'),
+            InlineKeyboardButton('🔐 Close', callback_data='close_data')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text=Translation.ABOUT_MSG.format(query.from_user.mention),
+            reply_markup=reply_markup,
+            parse_mode='html'
+        )
             
                 
 
