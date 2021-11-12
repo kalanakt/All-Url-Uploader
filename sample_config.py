43b85bdbdc1dcb480020c5252ebfcc3a32ac7915
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2021-11-12 09:29:19 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -9,14 +9,12 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH")
     # Get these values from my.telegram.org
     # Banned Unwanted Members..
-    BANNED_USERS = set(int(x)
-                       for x in os.environ.get("BANNED_USERS", "").split())
+    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
     MAX_FILE_SIZE = 50000000
     TG_MAX_FILE_SIZE = 2097152000
-    UPDATE_CHANNEL = 'TMWAD'
     FREE_USER_MAX_FILE_SIZE = 50000000
     # chunk size that should be used with requests
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
