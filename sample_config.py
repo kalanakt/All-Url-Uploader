e9753c2419ce1722bebbbaceaadef1219613e45c
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2021-11-12 09:41:12 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -35,5 +35,3 @@ class Config(object):
     SESSION_NAME = os.environ.get("SESSION_NAME", "")
     # database uri (mongodb)
     DATABASE_URL = os.environ.get("DATABASE_URL", "")
-    # logs of bot
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
