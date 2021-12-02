02d795591bb83963596f139c85048702f3ee0e3c
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2021-12-03 01:02:26 +0530

    Create __init__.py

---
+++ megadl/__init__.py
@@ -0,0 +1,14 @@
+from pyrogram import Client
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+plugins = dict(root="megadl/modules")
+meganzbot = Client(
+        "MegaBot",
+        bot_token=Config.TG_BOT_TOKEN,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        plugins=plugins
+    )
