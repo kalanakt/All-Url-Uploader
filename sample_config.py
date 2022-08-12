75333525c2a6efea203356e0f75f0193e8969876
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-08-12 19:08:56 +0530

    [ deploy in local | vps ]

--- sample_config.py
+++ sample_config.py
@@ -3,11 +3,11 @@
 class Config(object):
 
     # get a token from @BotFather
-    BOT_TOKEN = "5593440351:AAGuPzlgwqN0rgqmdL6kceiPRLrXFjt89Zg"
+    BOT_TOKEN = ""
 
     # Get these values from my.telegram.org
-    API_ID = 6129651
-    API_HASH = "12c5b64ffbd1425f2cf0f2910bc16ef5"
+    API_ID = 
+    API_HASH = ""
 
     # No need to change
     DOWNLOAD_LOCATION = "./DOWNLOADS"
@@ -18,11 +18,11 @@ class Config(object):
     PROCESS_MAX_TIMEOUT = 3700
 
     # TG Ids
-    LOG_CHANNEL = -1001718967421
-    OWNER_ID = 1288398723
+    LOG_CHANNEL =
+    OWNER_ID = 
 
     # bot username without @
-    BOT_USERNAME = "AdvanceUrlUploaderBot"
+    BOT_USERNAME = ""
 
     # auth users
-    AUTH_USERS = [OWNER_ID, OWNER_ID]
+    AUTH_USERS = [OWNER_ID,]
