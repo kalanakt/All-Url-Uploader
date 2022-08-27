0d15c3b3cc6c4eb0844acdf98f99d98abe217208
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2022-08-27 23:16:05 +0400

    .

--- Uploader/echo.py
+++ Uploader/echo.py
@@ -431,7 +431,7 @@ def down(message,link):
 
 
 # mdisk command
-@Client.on_message(filters.private & filters.regex(pattern="/mdisk*"))
+@Client.on_message(filters.private & filters.regex(pattern="/mdisk.*"))
 async def mdiskdown(bot, update):
     
     try:
