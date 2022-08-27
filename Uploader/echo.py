7e02315a3923848f04d8422e33bb7249a97a3b51
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2022-08-28 00:37:20 +0400

    status ?

--- Uploader/echo.py
+++ Uploader/echo.py
@@ -448,3 +448,18 @@ async def echo(bot, update):
         )
 
 
+@Client.on_message(filters.private & filters.command("status") & filters.user(Config.UTH_USERS))
+async def _status(_, m: Message):
+    total, used, free = shutil.disk_usage(".")
+    total = humanbytes(total)
+    used = humanbytes(used)
+    free = humanbytes(free)
+    cpu_usage = psutil.cpu_percent()
+    ram_usage = psutil.virtual_memory().percent
+    disk_usage = psutil.disk_usage('/').percent
+    total_users = await db.total_users_count()
+    await m.reply_text(
+        text=f"**Total Disk Space:** {total} \n**Used Space:** {used}({disk_usage}%) \n**Free Space:** {free} \n**CPU Usage:** {cpu_usage}% \n**RAM Usage:** {ram_usage}%\n\n**Total Users in DB:** `{total_users}`",
+        parse_mode="Markdown",
+        quote=True
+    )
