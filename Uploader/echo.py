c6ed3ce52eeca7c311eec7deb733d3505eb171bd
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2022-08-30 01:17:39 +0400

    update

--- Uploader/echo.py
+++ Uploader/echo.py
@@ -67,8 +67,8 @@ async def mdisk(bot, update):
     link = r.get("download")
     desc = r.get("filename")
     await update.reply_text(("your link: {}").format(link))
-    await bot.send_video(
-        chat,
+    await update.send_video(
+        chat_id=update.message.chat.id,
         video=link,
         caption=desc
     )
