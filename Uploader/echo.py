1feabdfdae71b3d0fb368e5772c45808a09c0658
kalana kithmina <e19198@eng.pdn.ac.lk>
2022-11-19 01:10:10 +0530

    Fix 'duplicate` Upload mode [ #85 ]
    video x2 --> [ video; documents ];

--- Uploader/echo.py
+++ Uploader/echo.py
@@ -273,7 +273,7 @@ async def echo(bot, update):
                 "video", format_id, format_ext, randem)
             inline_keyboard.append([
                 InlineKeyboardButton(
-                    "🎬 sᴍᴇᴅɪᴀ",
+                    "🎬 Video",
                     callback_data=(cb_string_video).encode("UTF-8")
                 )
             ])
@@ -283,8 +283,8 @@ async def echo(bot, update):
                 "video", format_id, format_ext)
             inline_keyboard.append([
                 InlineKeyboardButton(
-                    "🎥 ᴠɪᴅᴇᴏ",
-                    callback_data=(cb_string_video).encode("UTF-8")
+                    "📁 Document",
+                    callback_data=(cb_string_file).encode("UTF-8")
                 )
             ])
         reply_markup = InlineKeyboardMarkup(inline_keyboard)
