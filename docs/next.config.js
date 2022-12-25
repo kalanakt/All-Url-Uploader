76de5ec9dfe7ef42f72405c387ada4430556fab1
kalana kithmina <e19198@eng.pdn.ac.lk>
2022-12-26 00:39:59 +0530

    Update next.config.js

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,6 +1,7 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  basePath: '/All-Url-Uploader/docs',
 })
 
 module.exports = withNextra()
