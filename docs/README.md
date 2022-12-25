41ade64a7706fbc5e123c40a92ce61b7471a9930
kalanakt <e19198@eng.pdn.ac.lk>
2022-12-26 00:00:09 +0530

    replace docs with `next` `nextra`

---
+++ docs/README.md
@@ -0,0 +1,1 @@
+# all url uploader doc
---
+++ docs/theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>All-url-uploader</span>,
+  project: {
+    link: 'https://github.com/kalanakt/All-Url-Uploader',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/kalanakt/All-Url-Uploader/docs',
+  footer: {
+    text: '<em>Copyright (c) 2022-present <strong><a href="https://github.com/kalanakt">Hash Minner</a></strong></em>',
+  },
+}
+
+export default config
