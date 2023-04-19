105b5b950a55fe29ab56543d42041412d32959ef
kalana kithmina <e19198@eng.pdn.ac.lk>
2023-04-19 07:26:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,30 +39,16 @@ logger = logging.getLogger(__name__)
 
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-class Bot(Client):
 
-    def __init__(self):
-        super().__init__(
-            name='All-Url-Uploader',
-            api_id=Config.API_ID,
-            api_hash=Config.API_HASH,
-            bot_token=Config.BOT_TOKEN,
-            workers=50,
-            plugins={"root": "plugins"},
-            sleep_threshold=5,
-        )
+app = Client(
+    name='All-Url-Uploader',
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    bot_token=Config.BOT_TOKEN,
+    workers=50,
+    plugins={"root": "plugins"},
+    sleep_threshold=5,
+)
 
-    async def start(self):
-        await super().start()
-        me = await self.get_me()
-        logging.info(
-            f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
-
-    async def stop(self, *args):
-        await super().stop()
-        logging.info("Bot stopped. Bye.")
-
-
-app = Bot()
 app.run()
 #     logger.info("Bot Stoped ;)")
