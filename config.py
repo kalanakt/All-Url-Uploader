72c2e940eccc4945b39c78a2395ea355ce9a402f
7298ravi <RDJHindiofficialrg@gmail.com>
2024-10-02 15:45:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,10 +13,10 @@ load_dotenv()
 
 class Config(object):
     # Get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN"7848450856:AAGzCfGnAE71YU7VwJ5vm-3DLCDFgJM7Y54)
     # The Telegram API things
-    API_ID = os.environ.get("API_ID")
-    API_HASH = os.environ.get("API_HASH")
+    API_ID = os.environ.get("API_ID"25613025)
+    API_HASH = os.environ.get("API_HASH"b9c705fb515c81e90f239918f2f7f668)
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
 
@@ -30,12 +30,12 @@ class Config(object):
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     # Proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
-    HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
+    HTTP_PROXY = os.environ.get("HTTP_PROXY", "mongodb+srv://LEGEND_OF_ALL_TIME:4lD7yH5HwL0ONHwq@legend.giyqu.mongodb.net/?retryWrites=true&w=majority&appName=LEGEND")
 
     # Set timeout for subprocess
     PROCESS_MAX_TIMEOUT = 3700
 
-    OWNER_ID = os.environ.get("OWNER_ID")
+    OWNER_ID = os.environ.get("OWNER_ID"6620837238)
     ADL_BOT_RQ = {}
     AUTH_USERS = list({int(x) for x in os.environ.get("AUTH_USERS", "0").split()})
     AUTH_USERS.append(OWNER_ID)
